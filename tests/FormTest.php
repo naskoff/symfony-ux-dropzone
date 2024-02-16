18d8644b840af01e279f2ef8eb7496916f76c9d3
Zlatin Hristov <zlatin.hristov@softavis.com>
2024-02-16 21:08:22 +0200

    add github workflow, add base phpunit tests

---
+++ tests/FormTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FormTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
